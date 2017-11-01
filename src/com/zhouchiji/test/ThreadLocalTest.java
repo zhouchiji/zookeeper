829e9fa8f076168f3b0ef831e56c4a361e98f95b
zhouchiji <zhoucj@jkcmoney.com>
2017-11-01 14:50:15 +0800

    md

---
+++ src/com/zhouchiji/test/ThreadLocalTest.java
@@ -0,0 +1,29 @@
+package com.zhouchiji.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ThreadLocalTest {
+    private static ThreadLocal threadLocal = new ThreadLocal();
+
+    public static void main(String[] args) {
+        setMany();
+        Map<String, Object> map = (Map<String, Object>) threadLocal.get();
+        System.out.println(map);
+    }
+
+    public static void setValue(String key, Object value) {
+        Map<String, Object> map = (Map<String, Object>) threadLocal.get();
+        if (map == null) {
+            map = new HashMap<>();
+        }
+        map.put(key, value);
+        threadLocal.set(map);
+    }
+
+    public static void setMany() {
+        setValue("user", 1);
+        setValue("sex", 2);
+        setValue("age", 3);
+    }
+}
